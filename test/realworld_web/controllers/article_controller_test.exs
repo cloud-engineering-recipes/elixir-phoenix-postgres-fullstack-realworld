72230b7652bb1d7c4540f5666ca8de0aadb7c058
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2022-11-07 09:22:45 -0500

    Add list_articles route test case

--- test/realworld_web/controllers/article_controller_test.exs
+++ test/realworld_web/controllers/article_controller_test.exs
@@ -114,7 +114,7 @@ defmodule RealWorldWeb.ArticleControllerTest do
       assert updated_article["description"] == update_article_params.description
       assert updated_article["body"] == update_article_params.body
       assert updated_article["tagList"] == update_article_params.tagList
-      assert {:ok, _} = Date.from_iso8601(updated_article["createdAt"])
+      assert updated_article["createdAt"] == Date.to_iso8601(article.inserted_at)
       assert {:ok, _} = Date.from_iso8601(updated_article["updatedAt"])
       assert !updated_article["favorited"]
       assert updated_article["favoritesCount"] == 0
@@ -230,8 +230,8 @@ defmodule RealWorldWeb.ArticleControllerTest do
       assert user_1_article["description"] == article.description
       assert user_1_article["body"] == article.body
       assert user_1_article["tagList"] == article.tags
-      assert user_1_article["createdAt"] != nil
-      assert user_1_article["updatedAt"] != nil
+      assert user_1_article["createdAt"] == Date.to_iso8601(article.inserted_at)
+      assert user_1_article["updatedAt"] == Date.to_iso8601(article.updated_at)
       assert user_1_article["favorited"]
       assert user_1_article["favoritesCount"] == 1
 
@@ -328,8 +328,8 @@ defmodule RealWorldWeb.ArticleControllerTest do
       assert user_1_article["description"] == article.description
       assert user_1_article["body"] == article.body
       assert user_1_article["tagList"] == article.tags
-      assert user_1_article["createdAt"] != nil
-      assert user_1_article["updatedAt"] != nil
+      assert user_1_article["createdAt"] == Date.to_iso8601(article.inserted_at)
+      assert user_1_article["updatedAt"] == Date.to_iso8601(article.updated_at)
       assert user_1_article["favorited"]
       assert user_1_article["favoritesCount"] == 2
 
@@ -482,25 +482,139 @@ defmodule RealWorldWeb.ArticleControllerTest do
   end
 
   describe "list_articles" do
+    test "returns 200 and renders empty list of articles no articles exist", %{
+      conn: conn
+    } do
+      user = insert(:user)
+
+      list_articles_conn =
+        conn
+        |> secure_conn(user.id)
+        |> get(Routes.article_path(conn, :list_articles))
+
+      assert %{"articles" => articles, "articlesCount" => articles_count} =
+               json_response(list_articles_conn, 200)
+
+      assert Enum.empty?(articles)
+      assert articles_count == 0
+    end
+
+    test "returns 200 and renders multiple articles when given all filters", %{
+      conn: conn
+    } do
+      user1 = insert(:user)
+      user2 = insert(:user)
+      user3 = insert(:user)
+      tag1 = insert(:tag, name: "tag1")
+      tag2 = insert(:tag, name: "tag2")
+
+      limit = 3
+      offset = 1
+
+      article1 =
+        insert(:article,
+          author: user1,
+          tags: [tag1],
+          inserted_at: DateTime.utc_now() |> Timex.subtract(Timex.Duration.from_days(3))
+        )
+
+      article2 =
+        insert(:article,
+          author: user1,
+          tags: [tag1, tag2],
+          inserted_at: DateTime.utc_now() |> Timex.subtract(Timex.Duration.from_days(2))
+        )
+
+      article3 =
+        insert(:article,
+          author: user1,
+          tags: [tag1],
+          inserted_at: DateTime.utc_now() |> Timex.subtract(Timex.Duration.from_days(1))
+        )
+
+      article4 =
+        insert(:article,
+          author: user1,
+          tags: [tag1]
+        )
+
+      Enum.each([article1, article2, article3, article4], fn article ->
+        user2_favorite_article_conn =
+          conn
+          |> secure_conn(user2.id)
+          |> post(Routes.article_path(conn, :favorite_article, article.slug))
+
+        assert %{"article" => _} = json_response(user2_favorite_article_conn, 200)
+
+        user3_favorite_article_conn =
+          conn
+          |> secure_conn(user3.id)
+          |> post(Routes.article_path(conn, :favorite_article, article.slug))
+
+        assert %{"article" => _} = json_response(user3_favorite_article_conn, 200)
+      end)
+
+      user2_follow_user1_conn =
+        conn
+        |> secure_conn(user2.id)
+        |> post(Routes.profile_path(conn, :follow_user, user1.username))
+
+      assert %{"profile" => _} = json_response(user2_follow_user1_conn, 200)
+
+      list_articles_params = %{
+        tag: tag1.name,
+        author: user1.username,
+        favorited: user2.username,
+        limit: limit,
+        offset: offset
+      }
+
+      expected_articles = [
+        article3,
+        article2,
+        article1
+      ]
+
+      list_articles_conn =
+        conn
+        |> secure_conn(user2.id)
+        |> get(Routes.article_path(conn, :list_articles, list_articles_params))
+
+      assert %{"articles" => articles, "articlesCount" => articles_count} =
+               json_response(list_articles_conn, 200)
+
+      for i <- 0..(length(articles) - 1) do
+        article = Enum.at(articles, i)
+        expected_article = Enum.at(expected_articles, i)
+
+        assert article["slug"] == Slug.slugify(expected_article.title)
+        assert article["title"] == expected_article.title
+        assert article["description"] == expected_article.description
+        assert article["body"] == expected_article.body
+        assert article["tagList"] == expected_article.tags |> Enum.map(& &1.name)
+        assert article["createdAt"] == Date.to_iso8601(expected_article.inserted_at)
+        assert article["updatedAt"] == Date.to_iso8601(expected_article.updated_at)
+        assert article["favorited"]
+        assert article["favoritesCount"] == 2
+
+        assert article["author"] == %{
+                 "username" => user1.username,
+                 "bio" => user1.bio,
+                 "image" => user1.image,
+                 "following" => true
+               }
+      end
+
+      assert articles_count == length(expected_articles)
+    end
+
     test "returns 200 and renders 20 articles by default when limit is not set", %{
       conn: conn
     } do
-      for _ <- 0..21 do
-        author = insert(:user)
+      limit_by_default = 20
 
-        create_article_params = %{
-          title: Faker.Lorem.sentence(),
-          description: Faker.Lorem.sentence(),
-          body: Faker.Lorem.paragraph(),
-          tagList: Faker.Lorem.words()
-        }
-
-        create_article_conn =
-          conn
-          |> secure_conn(author.id)
-          |> post(Routes.article_path(conn, :create_article), article: create_article_params)
-
-        assert %{"article" => _} = json_response(create_article_conn, 201)
+      for _ <- 0..(limit_by_default + 5) do
+        insert(:article)
       end
 
       list_articles_conn =
@@ -510,8 +624,8 @@ defmodule RealWorldWeb.ArticleControllerTest do
       assert %{"articles" => articles, "articlesCount" => articles_count} =
                json_response(list_articles_conn, 200)
 
-      assert length(articles) == 20
-      assert articles_count == 20
+      assert length(articles) == limit_by_default
+      assert articles_count == limit_by_default
     end
   end
 end
